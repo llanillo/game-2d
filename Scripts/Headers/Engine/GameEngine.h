fdc841eb418072d165d2d9da312132697dd4f694
Octavio Llanillo <llanillo4@hotmail.com>
2022-07-16 10:12:14 -0300

    Create Game Engine class

---
+++ Scripts/Headers/Engine/GameEngine.h
@@ -0,0 +1,36 @@
+#ifndef INC_2D_GAMEENGINE_H
+#define INC_2D_GAMEENGINE_H
+
+class SDL_Window;
+class SDL_Renderer;
+class SDL_Texture;
+class SDL_Rect;
+
+class GameEngine {
+public:
+
+    GameEngine();
+    ~GameEngine();
+
+    void Init(const char* Title, int XPos, int YPos, int Width, int Height, bool Fullscreen);
+
+    void HandleEvents();
+    void Update();
+    void Render();
+    void Clean();
+
+    inline bool IsRunning() const { return bIsRunning; }
+
+private:
+
+    int Counter = 0;
+    bool bIsRunning;
+    SDL_Window* Window;
+    SDL_Renderer* Renderer;
+
+    SDL_Texture* PlayerTexture;
+    SDL_Rect* SourceRectangle, *DestinationRectangle;
+};
+
+
+#endif //INC_2D_GAMEENGINE_H
---
+++ Scripts/Source/Engine/GameEngine.cpp
@@ -0,0 +1,86 @@
+#include "Engine/GameEngine.h"
+#include <SDL.h>
+#include <iostream>
+#include <SDL_image.h>
+#include <SDL_rect.h>
+
+GameEngine::GameEngine(){
+
+
+}
+
+GameEngine::~GameEngine(){
+
+}
+
+void GameEngine::Init(const char *Title, int XPos, int YPos, int Width, int Height, bool Fullscreen) {
+    int Flags = 0;
+
+    if(Fullscreen){
+        Flags = SDL_WINDOW_FULLSCREEN;
+    }
+
+    if(SDL_Init(SDL_INIT_EVERYTHING) == 0){
+        std::cout << "Subsystems initialised...\n";
+        Window = SDL_CreateWindow(Title, XPos, YPos, Width, Height, Flags);
+
+        if(Window){
+            std::cout << "Window created\n";
+            Renderer = SDL_CreateRenderer(Window, -1, 0);
+
+            if(Renderer){
+                std::cout << "Renderer created\n";
+                SDL_SetRenderDrawColor(Renderer, 255, 255, 255 , 255);
+                bIsRunning = true;
+            }
+            else{
+                bIsRunning = false;
+            }
+        }
+        else{
+            bIsRunning = false;
+        }
+    }
+
+    SDL_Surface* TmpSurface = IMG_Load("Assets/Player/Player.png");
+    PlayerTexture = SDL_CreateTextureFromSurface(Renderer, TmpSurface);
+    SDL_FreeSurface(TmpSurface);
+
+    SourceRectangle = new SDL_Rect();
+    DestinationRectangle = new SDL_Rect();
+}
+
+void GameEngine::Render() {
+    SDL_RenderClear(Renderer);
+    SDL_RenderCopy(Renderer, PlayerTexture, nullptr, DestinationRectangle);
+    SDL_RenderPresent(Renderer);
+}
+
+void GameEngine::Update() {
+    Counter++;
+
+    DestinationRectangle->h = 64;
+    DestinationRectangle->w = 64;
+//    DestinationRectangle->x = Counter;
+}
+
+void GameEngine::Clean() {
+    SDL_DestroyWindow(Window);
+    SDL_DestroyRenderer(Renderer);
+    SDL_Quit();
+    std::cout << "Game Cleaned\n";
+}
+
+void GameEngine::HandleEvents(){
+    SDL_Event Event;
+    SDL_PollEvent(&Event);
+
+    switch(Event.type){
+        case SDL_QUIT:
+            bIsRunning = false;
+            break;
+        default:
+            break;
+    }
+}
+
