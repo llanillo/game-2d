d338117b5bd79d11eec7a3c20231402a40402901
Octavio Llanillo <llanillo4@hotmail.com>
2022-07-20 21:46:42 -0300

    Update GameObject.cpp

--- Scripts/Source/Template/GameObject.cpp
+++ Scripts/Source/Template/GameObject.cpp
@@ -1,9 +1,37 @@
 #include "Template/GameObject.h"
-#include <SDL.h>
+#include "Engine/GameEngine.h"
 #include "Engine/TextureManager.h"
+#include <SDL.h>
 
-GameObject::GameObject(const char* TextureSheet, SDL_Renderer* Renderer){
-    this->Renderer = Renderer;
-    Texture = TextureManager::LoadTexture(TextureSheet, Renderer);
+GameObject::GameObject(const char* TextureSheet, int X, int Y){
+    XPos = X;
+    YPos = Y;
+
+    SourceRect = new SDL_Rect();
+    DestRect = new SDL_Rect();
+    Texture = TextureManager::LoadTexture(TextureSheet);
 }
 
+GameObject::~GameObject(){
+
+}
+
+void GameObject::Render() {
+    SDL_RenderCopy(GameEngine::Renderer, Texture, nullptr, DestRect);
+}
+
+void GameObject::Update(double DeltaTime){
+//    XPos++;
+//    YPos++;
+
+    SourceRect->h = GameEngine::TileSize;
+    SourceRect->w = GameEngine::TileSize;
+    SourceRect->x = 0;
+    SourceRect->y = 0;
+
+    DestRect->x = XPos;
+    DestRect->y = YPos;
+    DestRect->w = SourceRect->w * 2;
+    DestRect->h = SourceRect->h * 2;
+
+}
